31505c1e1f8efdac7db9cbd37231d25e37337005
mandeep <singh.mr.1996@gmail.com>
2021-07-10 16:30:49 +0530

    user validation first name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,8 +1,18 @@
 package com.bridgelabz;
-public class UserRegistration 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration //class name
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) //main
 	{
-		System.out.println("Welcome To User Registration");
+		System.out.println("Welcome To User Registration");//welcome message
+		Scanner scanner = new Scanner(System.in); 
+		String FirstName  = scanner.next(); //user input
+		Pattern pattern = Pattern.compile("^([A-Z]{1}[a-z]{2,})");//Reg Exp
+		Matcher matcher = pattern.matcher(FirstName); //reg match with first_name
+		boolean isMatcher = matcher.matches();// boolean true/false
+		System.out.println(isMatcher);
 	}
 }
